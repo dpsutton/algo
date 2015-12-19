ec251b54cd33155fd5a6904450913d693fd54dbf
dan sutton <dan@localhost.localdomain>
2015-12-18 18:00:58 -0600

    include randomized partition

--- quicksort.py
+++ quicksort.py
@@ -1,6 +1,9 @@
+import random as random
+
+
 def quicksort(A, start, end):
     if start <= end:
-        pivot = partition(A, start, end)
+        pivot = randomized_partition(A, start, end)
         quicksort(A, start, pivot - 1)
         quicksort(A, pivot + 1, end)
 
@@ -15,3 +18,9 @@ def partition(A, start, end):
     pivotPosition = i + 1
     A[pivotPosition], A[end] = A[end], A[pivotPosition]
     return pivotPosition
+
+
+def randomized_partition(A, start, end):
+    i = random.randint(start, end)
+    A[i], A[end] = A[end], A[i]
+    return partition(A, start, end)
--- test_quicksort.py
+++ test_quicksort.py
@@ -27,6 +27,16 @@ class TestQuickSort(unittest.TestCase):
         self.assertEqual(expected, arr)
         self.assertEqual(pivot, 3)
 
+    def test_partition_checkInvariants(self):
+        arr = [4, 5, 2, 6, 7, 3, 9]
+        pivot = qs.partition(arr, 0, 6)
+        pivotValue = arr[pivot]
+        lessThan = [x <= pivotValue for x in arr[1:pivot]]
+        self.assertTrue(all(lessThan))
+
+        greaterThan = [x > pivotValue for x in arr[pivot + 1:]]
+        self.assertTrue(all(greaterThan))
+
     def test_quicksort_simplearray_sorts(self):
         arr = [5, 9, 3, 2, 8, 6]
         qs.quicksort(arr, 0, 5)
@@ -38,3 +48,13 @@ class TestQuickSort(unittest.TestCase):
         qs.quicksort(arr, 0, 299)
         expected = sorted(arr)
         self.assertEqual(expected, arr)
+
+    def test_randomizedquicksort_maintainsinvariants(self):
+        arr = self.get_values(200)
+        pivot = qs.randomized_partition(arr, 0, 199)
+        pivotValue = arr[pivot]
+        lessThan = [x <= pivotValue for x in arr[0:pivot]]
+        self.assertTrue(all(lessThan))
+
+        greaterThan = [x > pivotValue for x in arr[pivot + 1:]]
+        self.assertTrue(all(greaterThan))
