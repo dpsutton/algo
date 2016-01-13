fe165d18e83d6374074fb42898e48e7d19bc73e6
Dan Sutton <danielsutton01@gmail.com>
2016-01-12 22:23:07 -0600

    add test subheaders

--- setsInC.c
+++ setsInC.c
@@ -32,7 +32,9 @@ void testTree()
   tree two = {2, &one, NULL, NULL};
   tree four = {4, NULL, NULL, NULL};
   tree three = {3, &two, &four, NULL};
+  printSubHeader("Recursive descent through tree");
   tree_visitRecursive(&three);
+  printSubHeader("iterative journey through tree");
   tree_visitIterative(&three);
 }
 
